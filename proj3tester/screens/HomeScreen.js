8661b54c9c25208374bce7732a3d75b84c862654
bshephe9 <brookeshepherd672@gmail.com>
2019-01-23 00:29:41 -0500

    updated the home page

--- proj3tester/screens/HomeScreen.js
+++ proj3tester/screens/HomeScreen.js
@@ -5,27 +5,35 @@ import {
   View,
   ImageBackground,
 } from 'react-native';
+import Button from 'react-native-button';
 
 export default class HomeScreen extends React.Component {
   render() {
     return (
-        <ImageBackground
-          source={require('../assets/images/bg.jpg')}
-          style={styles.container}>
+      <ImageBackground
+        source={require('../assets/images/bg.jpg')}
+        style={styles.container}>
 
-          <View style={styles.overlayContainer}>
+        <View style={styles.overlayContainer}>
 
-            <View>
-              <Text style={styles.header}>enterVal</Text>
-            </View>
-
-            <View>
-              <Text style={styles.timer}>9:30 AM</Text>
-            </View>
-
+          <View>
+            <Text style={styles.header}>enterVal</Text>
           </View>
 
-        </ImageBackground>
+          <View>
+            <Text style={styles.timer}>9:30 am</Text>
+          </View>
+          <View>
+            <Button
+              style={{ fontSize: 20, color: '#fff', marginTop: 405, borderColor: '#fa8072', backgroundColor: '#fa8072', padding: 4, paddingLeft: 20, paddingRight: 20, borderRadius: 5, overflow: 'hidden' }}
+              styleDisabled={{ color: 'red' }}
+              onPress={() => this.doSomething()}>
+              get started
+            </Button>
+          </View>
+        </View>
+
+      </ImageBackground>
     );
   }
 }
@@ -37,25 +45,28 @@ const styles = StyleSheet.create({
     height: '100%',
   },
   overlayContainer: {
-    alignItems: 'center', 
-    color: 'black', 
-    marginTop: 120, 
-  }, 
-  header: { 
+    alignItems: 'center',
+    color: 'black',
+    marginTop: 120,
+  },
+  header: {
     fontSize: 35,
     borderColor: '#fff',
     fontWeight: 'bold',
     color: '#fff',
-    borderWidth: 2, 
-    padding: 20, 
-    paddingLeft: 40, 
-    paddingRight: 40, 
-  }, 
-  timer: { 
+    borderWidth: 2,
+    padding: 20,
+    paddingLeft: 40,
+    paddingRight: 40,
+  },
+  timer: {
     color: '#fff',
     fontSize: 25,
     marginTop: 20,
   },
+  button: {
+    marginTop: 400,
+  }
 });
 
 
--- proj3tester/screens/UserScreen.js
+++ proj3tester/screens/UserScreen.js
@@ -1,38 +1,64 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text} from 'react-native';
-import { Card } from 'react-native-elements';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
-export default class LinksScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Links',
-  };
+const styles = {
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
 
-  render() {
-    return (
-      <ScrollView style={styles.container}>
-        <Card> 
-          elevation={1}
-          shadowColor="rgb(50,50,50)"
-          shadowOpacity={1}
-          marginTop={25}
-          height={200}
-  
-        >
-          <Text style={{ padding: 10, fontSize: 18, textAlign: 'center'}}>
-            Insert Content Here 
-          </Text>
-        </Card>
+function SimpleCard(props) {
+  const { classes } = props;
+  const bull = <span className={classes.bullet}>•</span>;
 
-      </ScrollView>
-    );
-  }
+  return (
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography className={classes.title} color="textSecondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="h2">
+          be
+          {bull}
+          nev
+          {bull}o{bull}
+          lent
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          adjective
+        </Typography>
+        <Typography component="p">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </Card>
+  );
 }
 
+SimpleCard.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15,
-    backgroundColor: '#fff',
-  }
-});
+export default withStyles(styles)(SimpleCard);
